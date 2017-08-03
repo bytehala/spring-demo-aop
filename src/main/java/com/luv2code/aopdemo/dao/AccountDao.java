d7e982da54983be452dc1882d435bd9ef76937d5
Lem Dulfo <lemuel.dulfo@gmail.com>
2017-08-03 23:29:38 +0800

    Java-based spring config

---
+++ src/main/java/com/luv2code/aopdemo/dao/AccountDao.java
@@ -0,0 +1,12 @@
+package com.luv2code.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDao {
+	
+	public void addAccount() {
+		System.out.println(getClass() + ": Doing my DB work - adding an account");
+	}
+
+}
